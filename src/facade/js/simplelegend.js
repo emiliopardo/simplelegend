e165eb8cd82db8966aca48281ae921ef157f4801
emiliopardo <e.pardo.perez@gmail.com>
2021-09-15 14:27:06 +0200

    commit inicial

---
+++ src/facade/js/simplelegend.js
@@ -0,0 +1,74 @@
+/**
+ * @module M/plugin/Simplelegend
+ */
+import 'assets/css/simplelegend';
+import SimplelegendControl from './simplelegendcontrol';
+import api from '../../api.json';
+
+export default class Simplelegend extends M.Plugin {
+  /**
+   * @classdesc
+   * Main facade plugin object. This class creates a plugin
+   * object which has an implementation Object
+   *
+   * @constructor
+   * @extends {M.Plugin}
+   * @param {Object} impl implementation object
+   * @api stable
+   */
+  constructor() {
+    super();
+    /**
+     * Facade of the map
+     * @private
+     * @type {M.Map}
+     */
+    this.map_ = null;
+
+    /**
+     * Array of controls
+     * @private
+     * @type {Array<M.Control>}
+     */
+    this.controls_ = [];
+
+    /**
+     * Metadata from api.json
+     * @private
+     * @type {Object}
+     */
+    this.metadata_ = api.metadata;
+  }
+
+  /**
+   * This function adds this plugin into the map
+   *
+   * @public
+   * @function
+   * @param {M.Map} map the map to add the plugin
+   * @api stable
+   */
+  addTo(map) {
+    this.controls_.push(new SimplelegendControl());
+    this.map_ = map;
+    // panel para agregar control - no obligatorio
+    this.panel_ = new M.ui.Panel('panelSimplelegend', {
+      collapsible: true,
+      position: M.ui.position.TR,
+      collapsedButtonClass: 'g-cartografia-flecha-izquierda',
+    });
+    this.panel_.addControls(this.controls_);
+    map.addPanels(this.panel_);
+  }
+
+  /**
+   * This function gets metadata plugin
+   *
+   * @public
+   * @function
+   * @api stable
+   */
+  getMetadata(){
+    return this.metadata_;
+  }
+}
---
+++ test/test.js
@@ -0,0 +1,9 @@
+import Simplelegend from 'facade/simplelegend';
+
+const map = M.map({
+  container: 'mapjs',
+});
+
+const mp = new Simplelegend();
+
+map.addPlugin(mp);
