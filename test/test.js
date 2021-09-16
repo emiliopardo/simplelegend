294d591f1a8be1e952d81d71dc0ab83422b91489
emiliopardo <e.pardo.perez@gmail.com>
2021-09-16 14:03:35 +0200

    Se crean test de  configuracion

--- test/test.js
+++ test/test.js
@@ -4,6 +4,79 @@ const map = M.map({
   container: 'mapjs',
 });
 
-const mp = new Simplelegend();
+const configSimpleLegend ={
+  title: 'Leyenda',
+  layers: [
+    {
+      id: 1,
+      name: 'direst_transporte_2018',
+      title: 'Sector Transporte y almacenamiento',
+      style: 'direst_transporte',
+      url: 'http://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/direst/wms?',
+    },
+    {
+      id: 2,
+      name: 'direst_servicios_2018',
+      title: 'Sector Servicios sanitarios, educativos y resto de servicios',
+      style: 'direst_servicios',
+      url: 'http://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/direst/wms?',
+    },
+    {
+      id: 3,
+      name: 'direst_industria_2018',
+      title: 'Sector Industria',
+      style: 'direst_industria',
+      url: 'http://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/direst/wms?',
+    },
+    {
+      id: 4,
+      name: 'direst_hosteleria_2018',
+      title: 'Sector Hostelería',
+      style: 'direst_hosteleria',
+      url: 'http://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/direst/wms?',
+    },
+    {
+      id: 5,
+      name: 'direst_energia_2018',
+      title: 'Sector Energía, agua y resíduos',
+      style: 'direst_energia',
+      url: 'http://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/direst/wms?',
+    },
+    {
+      id: 6,
+      name: 'direst_construccion_2018',
+      title: 'Sector Construcción',
+      style: 'direst_construccion',
+      url: 'http://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/direst/wms?',
+    },
+    {
+      id: 7,
+      name: 'direst_comercio_2018',
+      title: 'Sector Comercio',
+      style: 'direst_comercio',
+      url: 'http://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/direst/wms?',
+    }
+  ]
+}
+
+
+const configSimpleLegend1 ={
+  title: 'Leyenda',
+  layers: [
+    {
+      id: 1,
+      name: 'direst_transporte_2018',
+      title: 'Sector Transporte y almacenamiento',
+      style: 'direst_transporte',
+      url: 'http://www.juntadeandalucia.es/institutodeestadisticaycartografia/geoserver-ieca/direst/wms?',
+    }
+  ]
+}
+const mp = new Simplelegend(configSimpleLegend);
+//const mp = new Simplelegend(configSimpleLegend1);
 
 map.addPlugin(mp);
+
+mp.on(M.evt.ADDED_TO_MAP, ()=>{
+  console.log('se cargo el plugin');
+})
