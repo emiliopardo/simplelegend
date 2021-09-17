202cd085f3513fc84754da0efd4f839b7a41ae93
emiliopardo <e.pardo.perez@gmail.com>
2021-09-17 13:45:28 +0200

    se crea logica drag u undateLegend

--- src/facade/js/simplelegendcontrol.js
+++ src/facade/js/simplelegendcontrol.js
@@ -27,6 +27,11 @@ export default class SimplelegendControl extends M.Control {
 
     this.title = config.title;
     this.layers = config.layers;
+    this.draggable = config.draggable;
+    this.pos1 = 0;
+    this.pos2 = 0;
+    this.pos3 = 0;
+    this.pos4 = 0;
     this.setLegend();
   }
 
@@ -49,10 +54,44 @@ export default class SimplelegendControl extends M.Control {
   }
 
   addEvents(html) {
+
+    this.legendTitle = html.querySelector('div#simple-legend-title');
+    this.legendBody = html.querySelector('div#simple-legend-body');
+
+    if (this.draggable) {
+      // console.log('es draggable')
+      this.legendTitle.style.cursor = 'move';
+    } else {
+      // console.log('no es dragable')
+      this.legendTitle.style.cursor = 'initial';
+    }
+
+    let mapId = this.getImpl().getMapId(this.map_);
     // QuerySelectors
 
+    this.mapDiv = document.getElementById(mapId);
+    this.legendPanel = document.getElementsByClassName('m-legend-panel')[0];
+
     // EventListener
- 
+    html.addEventListener('dragstart', (e) => {
+      //console.log('dragstart');
+      // get the mouse cursor position at startup:
+      this.pos3 = e.clientX;
+      this.pos4 = e.clientY;
+    })
+
+    this.mapDiv.addEventListener('dragover', (e) => {
+      //console.log('dragover');
+      // calculate the new cursor position:
+      this.pos1 = this.pos3 - e.clientX;
+      this.pos2 = this.pos4 - e.clientY;
+    })
+
+    html.addEventListener('dragend', (e) => {
+      //console.log('dragend')
+      html.style.top = (html.offsetTop - this.pos2) + "px";
+      html.style.left = (html.offsetLeft - this.pos1) + "px";
+    })
   }
 
 
@@ -81,20 +120,53 @@ export default class SimplelegendControl extends M.Control {
   }
 
   // Add your own functions
-  setLegend(){
+  setLegend() {
     let legendList = new Array()
     for (let index = 0; index < this.layers.length; index++) {
       const layer = this.layers[index];
       let legendElement = {
-        title :layer.title,
+        title: layer.title,
         name: layer.name,
         image: layer.url + 'service=WMS&version=1.1.1&request=GetLegendGraphic&format=image%2Fpng&width=20&height=20&layer=' + layer.name + '&style=' + layer.style
       }
       legendList.push(legendElement)
 
     }
-    this.templateVars = { vars: { title: this.title, legendElements: legendList} };
-    
+    this.templateVars = { vars: { title: this.title, draggable: this.draggable, legendElements: legendList } };
+  }
+
+  updateLegend(layers) {
+    if (Array.isArray(layers)) {
+      this.legendBody.innerHTML = '';
+      console.log('es array');
+      for (let index = 0; index < layers.length; index++) {
+        const layer = layers[index];
+        let imageURL = layer.url + 'service=WMS&version=1.1.1&request=GetLegendGraphic&format=image%2Fpng&width=20&height=20&layer=' + layer.options.params.layers + '&style=' + layer.options.params.styles
+
+        this.legendBody.innerHTML += '<div id="legend_' + layer.name + '" class="simple-legend-content">\n' +
+          '<label class="simple-legend-content-title">' + layer.legend + '</label>\n' +
+          '<img src="' + imageURL + '" alt="' + layer.legend + '" class ="simple-legend-content-image"></img>\n' +
+          '</div>';
+
+      }
+    } else {
+      let layer = layers;
+      console.log('no es array')
+      this.legendBody.innerHTML = '';
+      let imageURL = layer.url + 'service=WMS&version=1.1.1&request=GetLegendGraphic&format=image%2Fpng&width=20&height=20&layer=' + layer.options.params.layers + '&style=' + layer.options.params.styles
+
+      this.legendBody.innerHTML += '<div id="legend_' + layer.name + '" class="simple-legend-content">\n' +
+        '<label class="simple-legend-content-title">' + layer.legend + '</label>\n' +
+        '<img src="' + imageURL + '" alt="' + layer.legend + '" class ="simple-legend-content-image"></img>\n' +
+        '</div>';
+    }
+
+    //console.log(layers)
+
+
+
+
+
   }
 
 }
